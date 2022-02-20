545791e1e1fa4f90e08ecba4b5a63469e79f9005
EOussama <oussama_gtacj@live.fr>
2022-02-20 17:28:20 +0100

    🧩 Next.js initialization

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>EOussama</title>
+        <meta name="description" content="EOussama's portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
